2479733d2237f84e2d626f3125fe396bf570d6bf
TTHHKKYY <67774703+TTHHKKYY@users.noreply.github.com>
2021-10-31 13:53:47 -0600

    Update download.lua

--- src/download.lua
+++ src/download.lua
@@ -1,17 +1,25 @@
 local args = { ... }
 
 local script = "https://raw.githubusercontent.com/Black-Mesas/player-logger/main/src/main.lua"
+local installer = "https://raw.githubusercontent.com/Black-Mesas/player-logger/main/src/download.lua"
 local name = "logger.lua"
 
 local owner = args[1]
 
-if !fs.exists("/owner") and owner == nil then
+local function trim(s)
+   return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+if not fs.exists("/owner") and owner == nil then
     error("No owner found!")
 else
-    local ownerFile = fs.open("/owner", "w")
-    ownerFile.write(owner)
-    ownerFile.close()
+    local ownerFile = io.open("/owner", "w")
+    ownerFile:write(trim(owner))
+    ownerFile:close()
 end
+
+shell.run("rm download.lua")
+shell.run(string.format("wget %s download.lua",installer))
 
 shell.run(string.format("rm %s",name))
 shell.run(string.format("wget %s %s",script,name))
